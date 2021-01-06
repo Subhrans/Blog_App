f3c5e9882249b310039047481824eb802e559a42
subhransu das <subhransud525@gmail.com>
2021-01-06 18:05:44 +0530

    django-starter_template

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import  views
 urlpatterns = [
     path('',views.index,name="index")
 ]
+
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
